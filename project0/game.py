ccb7b78b3d4b9fa341e38076ab493be6ea0b9f43
Mana1496 <terentievamarianna@gmail.com>
2022-11-14 18:20:27 +0900

    edit game.ipynb

--- project0/game.py
+++ project0/game.py
@@ -1,9 +1,12 @@
 import numpy as np
-number = np.random.randint(1, 101)
+
+number = np.random.randint(1, 101) 
 count=0
+
 while True:
     count += 1
     predict_number = int(input("Угадай число от 1 до 100"))
+
     if predict_number > number:
         print("Число должно быть меньше!")
 
